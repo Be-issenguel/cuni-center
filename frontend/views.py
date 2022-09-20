b73f3ce5c9af4ede1a5a1b0434a8812b5ce7d412
Bernardo Issenguel <beissenguel12@gmail.com>
2022-09-20 07:32:16 +0100

    coming soon page

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def library_index(request):
+
     html = "<center><h1>Welcome to MeuGenio</h1></center>"
-    return HttpResponse(html)
+    context = {}
+    return render(request, 'frontend/coming-soon.html', context=context)
